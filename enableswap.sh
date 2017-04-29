ed418f79f1cf251f00f1b2323e8925f093c49c90
tnakamur <taka.kawajiri@gmail.com>
2017-04-29 15:15:57 +0900

    40.0.A.1.55

---
+++ enableswap.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo lz4 > /sys/block/zram0/comp_algorithm
+echo 768M > /sys/block/zram0/disksize
+#echo 0 > /sys/block/zram0/disksize
+/system/bin/mkswap /dev/block/zram0
+/system/bin/swapon /dev/block/zram0
